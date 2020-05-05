d5a66918a209425c11a874ae1c79644b111e8820
shane3606 <62545709+shane3606@users.noreply.github.com>
2020-05-05 16:52:39 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,6 +4,7 @@ from konlpy.tag import Kkma
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from gensim.models import doc2vec
 import multiprocessing
+import numpy as np
 
 
 kkma=Kkma()
@@ -15,33 +16,38 @@ def tokenize_kkma(sen):
     return token_sen
 
 
-def train(test_string):
+def train():
     token_faqs=[(tokenize_kkma(row[1]),row[0]) for row in sheet.sheets]
     tagged_faqs=[TaggedDocument(d,[c]) for d,c in token_faqs]
 
     cores=multiprocessing.cpu_count()
-    d2v_faqs=doc2vec.Doc2Vec(
-                        vector_size=50,
-                        alpha=0.025,
-                        min_alpha=0.025,
-                        hs=1,
-                        negative=0,
-                        dm=-0,
-                        dbow_words=1,
-                        min_count=1,
-                        workers=cores,
-                        seed=0
-                        )
+    d2v_faqs=doc2vec.Doc2Vec(vector_size=50,
+                            alpha=0.025,
+                            min_alpha=0.025,
+                            hs=1,
+                            negative=0,
+                            dm=-0,
+                            dbow_words=1,
+                            min_count=1,
+                            workers=cores,
+                            seed=0)
     d2v_faqs.build_vocab(tagged_faqs)
 
     for epoch in range(10):
         d2v_faqs.train(tagged_faqs,
                        total_examples=d2v_faqs.corpus_count,
                        epochs=d2v_faqs.epochs)
-    test_vector=d2v_faqs.infer_vector(tokened_test_string)
-    print(d2v_faqs.docvecs.most_similar([test_vector],topn=2))
+    return d2v_faqs
 
-
-test_string=input()
-tokened_test_string=tokenize_kkma(test_string)
-train(test_string)
+d2v=train()
+print("hello")
+while True:
+    test_string=input()
+    if test_string=="bye"or test_string=="Bye":
+        break
+    tokened_test_string=tokenize_kkma(test_string)
+    test_vector=d2v.infer_vector(tokened_test_string)
+    answer=d2v.docvecs.most_similar([test_vector],topn=1)
+    for index, similarity in answer:
+        print(sheet.sheets[int(index)-1][2])
+    
