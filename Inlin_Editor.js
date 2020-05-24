a14137ab73bd001e574d8c43a7a026bf65800e48
shane3606 <62545709+shane3606@users.noreply.github.com>
2020-05-25 03:11:51 +0900

    Update Inlin_Editor.js
    update Inline editor
    - add vector answer

--- Inlin_Editor.js
+++ Inlin_Editor.js
@@ -1,1 +1,55 @@
 //Inline editor
+const functions = require('firebase-functions');
+const admin=require('firebase-admin');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+admin.initializeApp({
+	credential: admin.credential.applicationDefault(),
+	databaseURL: 'ws://idechatbot-xsrbmv.firebaseio.com/'
+});
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function handleSTL(agent){
+  	const a=agent.parameters.STL;
+    return admin.database().ref().once("value").then((snapshot)=>{
+    	var STLInfo=snapshot.child('answer/'+a+'/idea').val();
+      	agent.add(STLInfo);
+    });
+  }
+  
+  function handlevector(agent){
+  	const a=agent.parameters.vector;
+    b=agent.parameters.function;
+    
+    if(b=="function"){b='funtion';}
+    else{b='idea';}
+    return admin.database().ref().once("value").then((snapshot)=>{
+    	var  vectorInfo=snapshot.child('answer/'+a+'/'+b).val();
+      	agent.add(vectorInfo);
+    });
+  }
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('STLIntent',handleSTL);
+  intentMap.set('vectorIntent',handlevector);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
