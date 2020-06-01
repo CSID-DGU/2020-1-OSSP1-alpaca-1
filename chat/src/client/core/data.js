517ba05938c32b338a193082e20d554f3a15c3ae
Lee-sun-ah <62545246+Lee-sun-ah@users.noreply.github.com>
2020-06-01 13:51:04 +0900

    Add files via upload

---
+++ chat/src/client/core/data.js
@@ -0,0 +1,10 @@
+const avatarUrls = [
+  'https://image.flaticon.com/icons/svg/2784/2784445.svg',
+  'https://image.flaticon.com/icons/png/512/2230/2230310.png',
+  'https://image.flaticon.com/icons/svg/2983/2983297.svg',
+  'https://image.flaticon.com/icons/png/512/2892/2892514.png',
+  'https://image.flaticon.com/icons/png/512/375/375111.png',
+  'https://image.flaticon.com/icons/svg/1998/1998642.svg',
+];
+
+export { avatarUrls };
